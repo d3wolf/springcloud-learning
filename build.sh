536cc3296c1d8b2958b7911fa8f65ef50e1f3e31
方良 <fangliang@xiaomi.com>
2020-04-25 22:11:02 +0800

    feat: build

--- build.sh
+++ build.sh
@@ -7,35 +7,35 @@ MODULE=Eureka-Server
 # eureka server
 cd ${WORKSPACE}/${MODULE}
 mvn clean package -DskipTests
-cp target/${MODULE}-0.0.1-SNAPSHOT.jar ${RUN_HOME}/${MODULE}-0.0.1-SNAPSHOT.jar
-java -jar ${RUN_HOME}/${MODULE}-0.0.1-SNAPSHOT.jar --Dspring.profiles.active=single
+cp target/${MODULE}-1.0-SNAPSHOT.jar ${RUN_HOME}/${MODULE}-1.0-SNAPSHOT.jar
+nohup java -jar java -jar ${RUN_HOME}/${MODULE}-1.0-SNAPSHOT.jar --Dspring.profiles.active=single &
 
 # provider
 MODULE=User-Provider
 cd ${WORKSPACE}/${MODULE}
 mvn clean package -DskipTests
-cp target/${MODULE}-0.0.1-SNAPSHOT.jar ${RUN_HOME}/${MODULE}-0.0.1-SNAPSHOT.jar
-java -jar ${RUN_HOME}/${MODULE}-0.0.1-SNAPSHOT.jar --Dspring.profiles.active=provider1
-java -jar ${RUN_HOME}/${MODULE}-0.0.1-SNAPSHOT.jar --Dspring.profiles.active=provider2
+cp target/${MODULE}-1.0-SNAPSHOT.jar ${RUN_HOME}/${MODULE}-1.0-SNAPSHOT.jar
+nohup java -jar ${RUN_HOME}/${MODULE}-1.0-SNAPSHOT.jar --Dspring.profiles.active=provider1 &
+nohup java -jar ${RUN_HOME}/${MODULE}-1.0-SNAPSHOT.jar --Dspring.profiles.active=provider2 &
 
 # consumer
 MODULE=User-Consumer
 cd ${WORKSPACE}/${MODULE}
 mvn clean package -DskipTests
-cp target/${MODULE}-0.0.1-SNAPSHOT.jar ${RUN_HOME}/${MODULE}-0.0.1-SNAPSHOT.jar
-java -jar ${RUN_HOME}/${MODULE}-0.0.1-SNAPSHOT.jar --Dspring.profiles.active=consumer1
-java -jar ${RUN_HOME}/${MODULE}-0.0.1-SNAPSHOT.jar --Dspring.profiles.active=consumer2
+cp target/${MODULE}-1.0-SNAPSHOT.jar ${RUN_HOME}/${MODULE}-1.0-SNAPSHOT.jar
+nohup java -jar ${RUN_HOME}/${MODULE}-1.0-SNAPSHOT.jar --Dspring.profiles.active=consumer1 &
+nohup java -jar ${RUN_HOME}/${MODULE}-1.0-SNAPSHOT.jar --Dspring.profiles.active=consumer2 &
 
 # admin
 MODULE=Admin-Test
 cd ${WORKSPACE}/${MODULE}
 mvn clean package -DskipTests
-cp target/${MODULE}-0.0.1-SNAPSHOT.jar ${RUN_HOME}/${MODULE}-0.0.1-SNAPSHOT.jar
-java -jar ${RUN_HOME}/${MODULE}-0.0.1-SNAPSHOT.jar
+cp target/${MODULE}-1.0-SNAPSHOT.jar ${RUN_HOME}/${MODULE}-1.0-SNAPSHOT.jar
+nohup java -jar ${RUN_HOME}/${MODULE}-1.0-SNAPSHOT.jar &
 
 # gateway
 MODULE=User-Gateway
 cd ${WORKSPACE}/${MODULE}
 mvn clean package -DskipTests
-cp target/${MODULE}-0.0.1-SNAPSHOT.jar ${RUN_HOME}/${MODULE}-0.0.1-SNAPSHOT.jar
-java -jar ${RUN_HOME}/${MODULE}-0.0.1-SNAPSHOT.jar
+cp target/${MODULE}-1.0-SNAPSHOT.jar ${RUN_HOME}/${MODULE}-1.0-SNAPSHOT.jar
+nohup java -jar ${RUN_HOME}/${MODULE}-1.0-SNAPSHOT.jar &
